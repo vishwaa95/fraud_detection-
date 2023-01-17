e81d9734f30d7f26c23416ed25c5d59f03c594d3
vishwaa95 <vishwajeeta95@hotmail.com>
2023-01-17 16:57:08 +0530

    added 3 new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print('adding all ml models here')
+
+def model1():
+    print('adding code for ml model1')
+
+def model2():
+    print('adding code for ml model2')
+
+def model3():
+    print('adding code for ml model3')
